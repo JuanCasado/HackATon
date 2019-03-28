ce3992aee75f90fb7ed8175955fb5fd2ebb5614c
Juan Casado Ballesteros <juan.casado@edu.uah.es>
2019-03-28 10:30:23 +0100

    0

---
+++ 0/src/pkg0/Main.java
@@ -0,0 +1,11 @@
+package pkg0;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+     ArrayList<String> arr= new ArrayList<>();
+     HashMap<String,String> map = new HashMap<>();
+    }
+}
